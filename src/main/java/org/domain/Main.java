0fc178d8b4f4d09cea4d08f7d663d83539507a0c
Igor-ua <694@ukr.net>
2017-09-04 12:10:47 +0300

    Initial commit

---
+++ src/main/java/org/domain/Main.java
@@ -0,0 +1,11 @@
+package org.domain;
+
+/**
+ * Entry point of the application
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("POM builder test");
+	}
+}
